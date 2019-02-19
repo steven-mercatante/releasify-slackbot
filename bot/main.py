bc3d89cca7ef41cb1bcde31020492110d717b0ab
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-18 21:51:49 -0500

    clean up

--- bot/main.py
+++ bot/main.py
@@ -6,10 +6,10 @@ from slackclient import SlackClient
 
 class PingResource(object):
     def on_get(self, req, resp):
-        resp.media = {"message": "ping"}
+        resp.media = {"message": "pong"}
 
     def on_post(self, req, resp):
-        resp.media = {"message": "ping"}
+        resp.media = {"message": "pong"}
 
 
 class ReleaseResource(object):
@@ -19,18 +19,9 @@ class ReleaseResource(object):
 
 def create_api():
     api = falcon.API()
+    api.add_route('/ping', PingResource())
     api.add_route('/release', ReleaseResource())
     return api
 
 
 api = create_api()
-
-
-
-# client = SlackClient(os.getenv('SLACK_TOKEN'))
-# print('>>>>', os.getenv('SLACK_TOKEN'))
-
-
-# if __name__ == '__main__':
-#     channels_call = client.api_call('channels.list')
-#     print(channels_call)
