e950a066761385cb99d0e4c9ea8a618dbee0d6e6
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-23 19:52:33 -0500

    create the release

--- bot/main.py
+++ bot/main.py
@@ -7,8 +7,7 @@ from pprint import pprint
 from textwrap import dedent
 
 import falcon
-import requests
-from releasify.client import Client as ReleasifyClient
+from releasify.client import ReleasifyClient
 from slackclient import SlackClient
 
 
@@ -41,155 +40,64 @@ class ReleaseResource(object):
         # TODO: check release_type is valid - return error otherwise
 
         # TODO: force should be passable as an arg. Or maybe show a button asking the user if they want to force it if there haven't been commits since the last release.
-        resp = releasify_client.create_release(
+        dry_run_resp = releasify_client.create_release(
             REPO_OWNER, 
             repo, 
             release_type, 
             dry_run=True, 
-            force_release=True
+            force=True
         )
-        # pprint(resp)
 
-        body = dedent(f"""
-        Version: *{resp['tag_name']}*
-        Body: *{resp['body']}*
-        Should I create this release? 
-        """)
-
-        # msg = {
-        #     'channel': req.get_param('channel_id'),
-        #     'user': req.get_param('user_id'),
-        #     'as_user': True,
-        #     "blocks": [{
-        #         "type": "section",
-        #         "text": {
-        #             "type": "mrkdwn",
-        #             "text": body,
-        #         },
-        #     }, {
-        #         "type": "actions",
-        #         "elements": [{
-        #             "type": "button",
-        #             "text": {
-        #                 "type": "plain_text",
-        #                 "text": "Yes",
-        #             },
-        #             "action_id": "confirm",
-        #             "value": json.dumps({
-        #                 'repo': repo,
-        #                 'release_type': release_type,
-        #             }),
-        #         }, {
-        #             "type": "button",
-        #             "text": {
-        #                 "type": "plain_text",
-        #                 "text": "No",
-        #             },
-        #             "action_id": "cancel",
-        #             "value": json.dumps({
-        #                 'message_id': 'foo'
-        #             }),
-        #         }]
-        #     }]
-        # }
-
+        state = json.dumps({
+            'repo': repo,
+            'release_type': release_type,
+        })
 
         dialog = json.dumps({
-            "callback_id": "create-release",
-            "title": "Create a Release",
-            "submit_label": "Create",
-            "notify_on_cancel": True,
-            "state": "Limo",
+            'callback_id': 'create-release',
+            'title': 'Create a Release',
+            'submit_label': 'Create',
+            'state': state,
             'elements': [{
                 'type': 'text',
                 'label': 'Tag',
                 'name': 'tag',
-                'value': resp['tag_name'],
+                'value': dry_run_resp['tag_name'],
             }, {
                 'type': 'textarea',
                 'label': 'Release body',
                 'name': 'body',
-                'value': resp['body'],
+                'value': dry_run_resp['body'],
             }]
         })
 
-        trigger_id = req.get_param('trigger_id')
-        print('>>>> trigger ID', trigger_id)
-
         resp = slack_client.api_call(
             'dialog.open',
-            trigger_id=trigger_id,
+            trigger_id=req.get_param('trigger_id'),
             dialog=dialog
         )
         pprint(resp)
 
-        # resp = slack_client.api_call('chat.postEphemeral', **msg)
-        # resp = slack_client.api_call('chat.postMessage', **msg)
 
-        # resp = requests.post(
-        #     req.get_param('response_url'),
-        #     data={
-        #         'response_type': 'ephemeral',
-        #         'text': 'lol'
-        #     }
-        # )
-
-        # print('>> orig msg ts:', resp['ts'])
-        # pprint(resp.content)
-        
-
+# TODO: rename ButtonHandlerResource
 class ButtonHandlerResource(object):
     def on_post(self, req, resp):
         payload = json.loads(req.get_param('payload'))
         pprint(payload)
 
-        action_id = payload['actions'][0]['action_id']
-        action_value = json.loads(payload['actions'][0]['value'])
-        # print('>>>>> action_id:', action_id)
-        # print('>>>>> action_value:', action_value)
+        state = json.loads(payload['state'])
+        release_tag = payload['submission']['tag']
+        release_body = payload['submission']['body']
+        resp = releasify_client.create_release(
+            REPO_OWNER,
+            state['repo'],
+            state['release_type'],
+            next_tag=release_tag,
+            body=release_body,
+        )
+        pprint(resp)
 
-        # TODO: if action_id is `cancel`, delete the prev message?
-
-        response_text = ''
-
-        if action_id == 'cancel':
-            print('DELETE THIS.....', payload['channel']['id'], payload['container']['message_ts'])
-            slack_client.api_call(
-                'chat.postMessage',
-                channel=payload['channel']['id'],
-                text="done!!!!",
-            )
-            # foo = slack_client.api_call(
-            #     'chat.delete', 
-            #     channel=payload['channel']['id'], 
-            #     ts=payload['container']['message_ts'],
-            #     as_user=True
-            # )
-            # pprint(foo)
-
-        # if action_id == 'confirm':
-        #     repo = action_value['repo']
-        #     release_type = action_value['release_type']
-        #     try:
-        #         resp = releasify_client.create_release(
-        #             REPO_OWNER, 
-        #             repo, 
-        #             release_type, 
-        #         )
-        #         pprint(resp)
-        #         response_text = 'success!!!'
-        #     except Exception as e:
-        #         response_text = f'Error: Unable to create {release_type} release. {str(e)}'
-
-        # resp = slack_client.api_call(
-        #     'chat.postMessage', 
-        #     channel=payload['channel']['id'],
-        #     as_user=True,
-        #     text=response_text,
-        # )
-        # print('``````````````````````````````')
-        # pprint(resp)
-        # resp.media = payload 
+        # TODO: post message in channel upon success
 
 
 def create_api():
